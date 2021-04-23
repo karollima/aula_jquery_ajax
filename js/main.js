014edf5ab4219d03a94fcda3acf08a7823b1440d
karollima <anne.karol.lgn@gmail.com>
2021-04-23 15:38:30 -0300

    finalizando aula jquery e ajax

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+function consultaCep(){
+    $(".barra-progresso").show();
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+    console.log(url);
+
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#titulo_cep").html("CEP " + response.cep);
+            $(".cep").show();
+            $(".barra-progresso").hide();
+        
+            //$("#logradouro").html(response.logradouro);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            
+            //alert(response.logradouro);
+        }
+
+    })
+}
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+});
